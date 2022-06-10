0a9755a229a42986ffa0eee8c60503411c974727
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2022-06-09 23:10:52 -0400

    feat: add to_default_format to commanded id

--- apps/one_piece_commanded/lib/one_piece/commanded/id.ex
+++ apps/one_piece_commanded/lib/one_piece/commanded/id.ex
@@ -11,4 +11,17 @@ defmodule OnePiece.Commanded.Id do
   def new do
     Uniq.UUID.uuid4(:hex)
   end
+
+  @doc """
+  Transform the UUID into the default format.
+
+  Useful when to transform a uuid with `:hex` format into a uuid with `:default` format.
+
+      iex> OnePiece.Commanded.Id.to_default_format("f81d4fae7dec11d0a76500a0c91e6bf6")
+      "f81d4fae-7dec-11d0-a765-00a0c91e6bf6"
+  """
+  @spec to_default_format(uuid :: String.t()) :: String.t()
+  def to_default_format(uuid) do
+    Uniq.UUID.to_string(uuid, :default)
+  end
 end
---
+++ apps/one_piece_commanded/test/one_piece/commanded/id_test.exs
@@ -0,0 +1,4 @@
+defmodule OnePiece.Commanded.IdTest do
+  use ExUnit.Case, asyc: true
+  doctest OnePiece.Commanded.Id
+end
