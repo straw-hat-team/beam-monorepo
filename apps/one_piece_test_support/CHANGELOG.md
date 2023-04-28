6b2361111984410ed11081696b3c9de2757105c6
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2023-04-28 02:49:09 -0400

    feat: add OnePiece.TestSupport package

---
+++ apps/one_piece_test_support/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## Unreleased
+
+## v0.1.0 - 2023-04-28
+
+- Initial release
---
+++ apps/one_piece_test_support/lib/one_piece/test_support.ex
@@ -0,0 +1,5 @@
+defmodule OnePiece.TestSupport do
+  @moduledoc """
+  A Swiss Army Knife for the Testing.
+  """
+end
