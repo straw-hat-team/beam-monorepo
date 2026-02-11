0235919078ad1b8f1292387399db85f836242d25
Yordis Prieto <yordis.prieto@gmail.com>
2026-02-11 13:01:26 -0500

    feat(trogon_proto): upgrade trogon-proto to v0.6.0 and regenerate (#311)
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

---
+++ apps/trogon_proto/lib/__generated__/trogon/object_id/v1alpha1/options.pb.ex
@@ -0,0 +1,63 @@
+defmodule TrogonProto.ObjectId.V1Alpha1.EnumValueOptions do
+  @moduledoc false
+
+  use Protobuf,
+    full_name: "trogon.object_id.v1alpha1.EnumValueOptions",
+    protoc_gen_elixir_version: "0.16.0",
+    syntax: :proto3
+
+  def descriptor do
+    # credo:disable-for-next-line
+    %Google.Protobuf.DescriptorProto{
+      name: "EnumValueOptions",
+      field: [
+        %Google.Protobuf.FieldDescriptorProto{
+          name: "object_type",
+          extendee: nil,
+          number: 1,
+          label: :LABEL_OPTIONAL,
+          type: :TYPE_STRING,
+          type_name: nil,
+          default_value: nil,
+          options: nil,
+          oneof_index: nil,
+          json_name: "objectType",
+          proto3_optional: nil,
+          __unknown_fields__: []
+        },
+        %Google.Protobuf.FieldDescriptorProto{
+          name: "separator",
+          extendee: nil,
+          number: 2,
+          label: :LABEL_OPTIONAL,
+          type: :TYPE_STRING,
+          type_name: nil,
+          default_value: nil,
+          options: nil,
+          oneof_index: 0,
+          json_name: "separator",
+          proto3_optional: true,
+          __unknown_fields__: []
+        }
+      ],
+      nested_type: [],
+      enum_type: [],
+      extension_range: [],
+      extension: [],
+      options: nil,
+      oneof_decl: [
+        %Google.Protobuf.OneofDescriptorProto{
+          name: "_separator",
+          options: nil,
+          __unknown_fields__: []
+        }
+      ],
+      reserved_range: [],
+      reserved_name: [],
+      __unknown_fields__: []
+    }
+  end
+
+  field(:object_type, 1, type: :string, json_name: "objectType")
+  field(:separator, 2, proto3_optional: true, type: :string)
+end
