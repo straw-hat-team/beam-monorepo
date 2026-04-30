38d93147c2447d913c3e1050a149d23d2c045f0a
Yordis Prieto <yordis.prieto@gmail.com>
2026-04-30 12:56:10 -0400

    chore: remove one_piece_graceful_shutdown (#364)
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- apps/one_piece_graceful_shutdown/lib/one_piece/graceful_shutdown/state.ex
+++
@@ -1,9 +0,0 @@
-defmodule OnePiece.GracefulShutdown.State do
-  @moduledoc false
-
-  defstruct init_stop?: true, shutdown_delay_ms: 10_000, notify_pid: nil
-
-  def new(opts) do
-    Map.merge(%__MODULE__{}, Enum.into(opts, %{}))
-  end
-end
